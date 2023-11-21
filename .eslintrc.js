3d68c2bdb8088f4e5fe95768a252b9b4701834df
liguodi <guodi_li@fpi-inc.com>
2023-11-21 19:42:31 +0800

    feat: eslint处理

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,9 @@ module.exports = {
     extends: ['@antfu'],
     rules: {
         // eslint 空格处理
-        'indent': ['error', 4], // 4个空格缩进
+        'indent': ['error', 4],
         '@stylistic/ts/indent': ['error', 4],
         'vue/html-indent': ['error', 4],
+        'jsonc/indent': ['error', 4],
     },
 }
