403dff3b83cde8fa7180a4c40e175ad236080277
liguodi <guodi_li@fpi-inc.com>
2023-11-21 17:14:28 +0800

    feat: 添加lint工具

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['@antfu'],
+    rules: {
+        // eslint 空格处理
+        'indent': ['error', 4], // 4个空格缩进
+        '@stylistic/ts/indent': ['error', 4],
+        'vue/html-indent': ['error', 4],
+    },
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
+import { URL, fileURLToPath } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,13 +6,13 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    plugins: [
+        vue(),
+        vueJsx(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
 })
