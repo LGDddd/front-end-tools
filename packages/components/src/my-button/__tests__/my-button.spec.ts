1c15745c03493bc6607f072625ac26582919feef
liguodi <guodi_li@fpi-inc.com>
2023-11-21 21:35:36 +0800

    提交代码

---
+++ packages/components/src/my-button/__tests__/my-button.spec.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from 'vitest'
+import { mount } from '@vue/test-utils'
+import Comp from '../index.vue'
+
+describe('my-button', () => {
+    it('renders properly', () => {
+        const wrapper = mount(Comp)
+        expect(wrapper.text()).toContain('ok')
+    })
+})
